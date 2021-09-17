d97939cb1bcc0c0f8b3fba52b82bdaf59b46fdf3
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-09-18 00:09:27 +0300

    a couple functions fixed

--- Bank.js
+++ Bank.js
@@ -2,45 +2,49 @@ const Bank = {};
 const Validation = require('./Validation');
 
 Bank.create = async (connection, currency, firstname, lastname, countryCode, defaultAcc, amount) => {
-    const sql = 'INSERT INTO klientai (id, firstname, lastname, country_code, default_bank_account_number)\
+    const insertKlientai = 'INSERT INTO klientai (id, firstname, lastname, country_code, default_bank_account_number)\
     VALUES (NULL, "' + firstname + '", "' + lastname + '","' + countryCode + '","' + defaultAcc + '")';
-    const [result] = await connection.execute(sql);
-    const sql1 = 'SELECT klientai.id\
+    const [result] = await connection.execute(insertKlientai);
+    const sql = 'SELECT klientai.id\
                     FROM klientai\
                         LEFT JOIN saskaitos\
                         ON klientai.id = saskaitos.user_id';
-    const [result1] = await connection.execute(sql1);
-    const sql2 = 'INSERT INTO saskaitos (id, user_id, country_code, bank_account_numbers, amount, currency)\
-    VALUES (NULL,"'+ sql1 + '", "' + countryCode + '", "' + defaultAcc + '", "' + amount + '","' + currency + '")';
-    const [result3] = await connection.execute(sql2);
+    const [result1] = await connection.execute(sql);
+    const insertSaskaitos = 'INSERT INTO saskaitos (id, user_id, country_code, bank_account_numbers, amount, currency)\
+    VALUES (NULL,"'+ result1[0].id + '", "' + countryCode + '", "' + defaultAcc + '", "' + amount + '","' + currency + '")';
+    const [result3] = await connection.execute(insertSaskaitos);
+
+
+
+
     return `${firstname} ${lastname} was successfully registered in client list with ${countryCode}${defaultAcc}.`
 }
 
 Bank.addAccounts = async (connection, userID, countryCode, bankAcc, amount, currency) => {
-    const sql = 'INSERT INTO saskaitos (id, user_id, country_code, bank_account_numbers, amount, currency) \
+    const insert = 'INSERT INTO saskaitos (id, user_id, country_code, bank_account_numbers, amount, currency) \
     VALUES(NULL, "'+ userID + '", "' + countryCode + '", "' + bankAcc + '", "' + amount + '", "' + currency + '")';
-    const [result] = await connection.execute(sql);
+    const [result] = await connection.execute(insert);
     return `New account ${countryCode}${bankAcc} of client, by ID = ${userID}, has been successfully registered with ${amount} ${currency}.`
 }
 
 Bank.depositMoney = async (connection, accountID, amount) => {
-    const sql = 'UPDATE saskaitos SET amount = amount + "' + amount + '" WHERE saskaitos.id =' + accountID;
-    const [result] = await connection.execute(sql);
-    const sql1 = 'SELECT saskaitos.currency FROM saskaitos WHERE id=1';
-    const [result1] = await connection.execute(sql1);
-    const sql3 = 'SELECT saskaitos.amount FROM saskaitos WHERE id=' + accountID;
-    const [result3] = await connection.execute(sql3);
+    const deposit = 'UPDATE saskaitos SET amount = amount + "' + amount + '" WHERE saskaitos.id =' + accountID;
+    const [result] = await connection.execute(deposit);
+    const currency = 'SELECT saskaitos.currency FROM saskaitos WHERE id=1';
+    const [result1] = await connection.execute(currency);
+    const balance = 'SELECT saskaitos.amount FROM saskaitos WHERE id=' + accountID;
+    const [result3] = await connection.execute(balance);
     return `${amount} ${result1[0].currency} have been added to bank account, by ID = ${accountID}, making a total of ${result3[0].amount} ${result1[0].currency}`
 
 }
 
 Bank.withdrawMoney = async (connection, accountID, amount) => {
-    const sql = 'UPDATE saskaitos SET amount = amount - "' + amount + '" WHERE saskaitos.id =' + accountID;
-    const [result] = await connection.execute(sql);
-    const sql1 = 'SELECT saskaitos.currency FROM saskaitos WHERE id=1';
-    const [result1] = await connection.execute(sql1);
-    const sql3 = 'SELECT saskaitos.amount FROM saskaitos WHERE id=' + accountID;
-    const [result3] = await connection.execute(sql3);
+    const withdraw = 'UPDATE saskaitos SET amount = amount - "' + amount + '" WHERE saskaitos.id =' + accountID;
+    const [result] = await connection.execute(withdraw);
+    const currency = 'SELECT saskaitos.currency FROM saskaitos WHERE id=1';
+    const [result1] = await connection.execute(currency);
+    const balance = 'SELECT saskaitos.amount FROM saskaitos WHERE id=' + accountID;
+    const [result3] = await connection.execute(balance);
     return `${amount} ${result1[0].currency} have been withdrawn from bank account, by ID = ${accountID}, making a total of ${result3[0].amount} ${result1[0].currency}`
 
 }
