b36b27ff09e26710bdb446d80f577b5437316ef2
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-09-17 18:59:33 +0300

    creating database and table for bank clients

---
+++ db.js
@@ -0,0 +1,57 @@
+const mysql = require('mysql2/promise');
+
+const db = {}
+
+db.init = async ({ database, host, user }) => {
+    const connection = await db.createDatabase({ database, host, user });
+
+    await db.createBankClientTable(connection);
+
+
+    return connection;
+}
+
+db.createDatabase = async ({ database, host, user }) => {
+    host = host ? host : 'localhost';
+    user = user ? user : 'root';
+
+    try {
+        let db = await mysql.createConnection({ host, user });
+        await db.execute(`DROP DATABASE IF EXISTS \`${database}\``);
+        console.log('Buvusi duombaze istrinta');
+    } catch (error) {
+        console.log('Nera duombazes, kuria butu galima istrinti');
+    }
+
+    try {
+        let db = await mysql.createConnection({ host, user });
+        await db.execute(`CREATE DATABASE IF NOT EXISTS \`${database}\``);
+        await db.end();
+
+        db = await mysql.createConnection({ host, user, database });
+        console.log('Nauja duombaze sukurta');
+        return db;
+    } catch (error) {
+        return error;
+    }
+}
+
+db.createBankClientTable = async (connection) => {
+    try {
+        const sql = 'CREATE TABLE IF NOT EXISTS `bankas` (\
+                        `id` int(10) NOT NULL AUTO_INCREMENT,\
+                        `firstname` char(20) COLLATE utf8_swedish_ci NOT NULL,\
+                        `lastname` char(20) COLLATE utf8_swedish_ci NOT NULL,\
+                        `amount` decimal(10,2) NOT NULL,\
+                        `currency` char(10) COLLATE utf8_swedish_ci NOT NULL,\
+                        PRIMARY KEY(`id`)\
+                    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_swedish_ci';
+        await connection.execute(sql);
+    } catch (error) {
+        console.log('Nepavyko sukurti banko klientu lenteles');
+        console.log(error);
+        return error;
+    }
+}
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const db = require('./db');
+const Bank = require('./Bank');
 
+const app = {}
+
+app.init = async () => {
+    // prisijungti prie duomenu bazes
+    const conn = await db.init({
+        host: 'localhost',
+        user: 'root',
+        database: 'bank',
+    });
+
+    // LOGIC BELOW
+
+}
+
+app.init();
+
+module.exports = app;
